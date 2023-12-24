3c0c606c9d2a434372d54ce10de92a5b861bb29d
dennis-limontov <dennis.limontov@gmail.com>
2023-12-24 22:51:13 +0300

    Added Railway Station scene

---
+++ Rock Voyage/Assets/Scripts/ScriptableObjects/HouseInfo/RailwayStationInfo.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace RockVoyage
+{
+    [CreateAssetMenu(menuName = "ScriptableObjects/HouseInfo/RailwayStationInfo")]
+    public class RailwayStationInfo : HouseInfo
+    {
+    }
+}
